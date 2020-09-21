7d5d7b63f32da09076a47f8e128d9154ebe39923
AppleArcade120 <32800772+AppleArcade120@users.noreply.github.com>
2020-09-21 17:18:08 -0600

    Add files via upload

---
+++ Wave/Modules/Options.Prefs.lua
@@ -0,0 +1,6 @@
+	SoundwavesSubTheme =
+	{
+		Default = 1,
+		Choices = { OptionNameString('swClassic'), OptionNameString('swVaporwave'), OptionNameString('swGrass'), OptionNameString('swRetro'), OptionNameString('swFire'), OptionNameString('swWave')},
+		Values = {1,2,3,4,5,6}
+	},
