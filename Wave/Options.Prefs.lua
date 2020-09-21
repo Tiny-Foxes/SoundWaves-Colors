1945d728a0a87e23b1107847d2e23be5d03065bc
AppleArcade120 <32800772+AppleArcade120@users.noreply.github.com>
2020-09-21 17:17:45 -0600

    Delete Options.Prefs.lua

--- Wave/Options.Prefs.lua
+++
@@ -1,6 +0,0 @@
-	SoundwavesSubTheme =
-	{
-		Default = 1,
-		Choices = { OptionNameString('swClassic'), OptionNameString('swVaporwave'), OptionNameString('swGrass'), OptionNameString('swRetro'), OptionNameString('swFire'), OptionNameString('swWave')},
-		Values = {1,2,3,4,5,6}
-	},
