e6c34ac14cdc1a6fdd7b7059f3719a3551cb67c5
Jack5 <jack5answers@gmail.com>
2021-05-10 11:10:58 +1000

    Rename opsm1 to Y2K

--- opsm1/Modules/Options.Prefs.lua
+++
@@ -1,6 +0,0 @@
-	SoundwavesSubTheme =
-	{
-		Default = 1,
-		Choices = { OptionNameString('swClassic'), OptionNameString('swVaporwave'), OptionNameString('swGrass'), OptionNameString('swRetro'), OptionNameString('swFire'), "Dark", "Chaos", "Ice", "Wave", "Alien Alien", "opsm1"},
-		Values = {1,2,3,4,5,6,7,8,9,10,11}
-	},
